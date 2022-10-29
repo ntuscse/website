60ca9f6373249035a95f5909050a644e5159c0b0
Dyllon <dyllon.g@outlook.com>
2022-10-29 11:10:55 +0800

    Add apps

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Web!</a>
+          Welcome to <a href="https://nextjs.org">IT Subcomm Workshop!</a>
         </h1>
 
         <p className={styles.description}>
