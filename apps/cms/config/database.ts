7fe002a3e2a2300afa374ec530e59c827fbc45c1
jamiegoh <jamiegoh16@gmail.com>
2022-11-03 02:54:58 +0800

    initialise strapi

--- apps/cms/config/database.ts
+++ apps/cms/config/database.ts
@@ -1,11 +1,13 @@
-import path from 'path';
-
 export default ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'cms'),
+      user: env('DATABASE_USERNAME', 'admin'),
+      password: env('DATABASE_PASSWORD', 'admin'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
-    useNullAsDefault: true,
   },
 });
