442e1e5e5ceb509d4cedcdae543ea3a2cebde4e1
Dyllon <dyllon.g@outlook.com>
2022-11-03 04:53:40 +0800

    Add cypress e2e tests

---
+++ apps/web/cypress/e2e/app.cy.ts
@@ -0,0 +1,24 @@
+describe('Navigation', () => {
+  it('should navigate to the about page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3001/')
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="about"]').click()
+
+    // The new url should include "/about"
+    cy.url().should('include', '/about')
+
+    // The new page should contain an h1 with "About page"
+    cy.get('h1').contains('About Page')
+
+    // Find a link with an href attribute containing "/" and click it
+    cy.get('a[href*="/"]').click()
+
+    // The new page should contain an h1 with "Welcome to Next.js!"
+    cy.get('h1').contains('Welcome to Next.js!')
+  })
+})
+
+// Prevent TypeScript from reading file as legacy script
+export {}
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,19 @@
 module.exports = {
   extends: ["next/core-web-vitals", "turbo", "prettier"],
+  plugins: [
+    "cypress"
+  ],
   rules: {
+    //  next
     "@next/next/no-html-link-for-pages": "off",
+    // react
     "react/jsx-key": "off",
+    // cypress
+    "cypress/no-assigning-return-values": "error",
+    "cypress/no-unnecessary-waiting": "error",
+    "cypress/assertion-before-screenshot": "warn",
+    "cypress/no-force": "warn",
+    "cypress/no-async-tests": "error",
+    "cypress/no-pause": "error"
   },
 };
