a03ade58a92e2c3c5e39ef401fee60f8b187ba23
Jing Qiang <tohjingqiang@gmail.com>
2022-11-07 15:34:47 +0800

    add 2 primary button styles and test in storybook

--- packages/ui/components/button/Button.stories.tsx
+++ packages/ui/components/button/Button.stories.tsx
@@ -13,8 +13,18 @@ export default {
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
+export const PrimaryBlue = Template.bind({});
+PrimaryBlue.args = {
+  label: 'CLICK ME',
+  href: '#',
+  size: 'lg',
+  buttonType: 'primary.blue',
+};
+
+export const PrimaryBlack = Template.bind({});
+PrimaryBlack.args = {
+  label: 'CLICK ME',
+  href: '#',
+  size: 'lg',
+  buttonType: 'primary.black',
 };
--- packages/ui/components/button/Button.tsx
+++ packages/ui/components/button/Button.tsx
@@ -1,8 +1,63 @@
-import { Button as BaseButton } from "@chakra-ui/react";
+import { Button as BaseButton, Link, Text } from "@chakra-ui/react";
+import { HTMLChakraProps, ThemingProps } from "@chakra-ui/system";
+import { ButtonOptions } from "@chakra-ui/button";
 
-export interface ButtonProps {}
+export interface ButtonProps extends HTMLChakraProps<"button">, ButtonOptions, ThemingProps<"Button"> {
+    label: string
+    href: string
+    size?: 'xs' | 'sm' | 'md' | 'lg'
+    buttonType?: 'primary.blue' | 'primary.black'
+}
 
-export const Button = (_props: ButtonProps) => {
-  return <BaseButton>I am a button
-  </BaseButton>
+export const Button = ({ buttonType='primary.blue', ...props }: ButtonProps) => {
+    let button;
+    switch (buttonType) {
+        case 'primary.black':
+            button = <PrimaryBlackButton {...props} />
+            break
+        default:
+            button = <PrimaryBlueButton {...props} />
+  }
+
+  return button
+}
+
+// Different Button Styles
+const PrimaryBlueButton = ({ label, href, size='lg', ...props }: ButtonProps) => {
+  return (
+      <BaseButton
+          key={label}
+          size={size}
+          rounded={'none'}
+          bg={'blue.600'}
+          color={'white'}
+          _hover={{ bg: 'white', color: 'black' }}
+          fontFamily={"Verdana Light"}
+          px={12}
+          py={8}
+          {...props}>
+          <Link href={href} _hover={{textDecoration: 'none'}}>
+              <Text>{label}</Text>
+          </Link>
+      </BaseButton>
+  )
+}
+const PrimaryBlackButton = ({ label, href, size='lg', ...props }: ButtonProps) => {
+  return (
+      <BaseButton
+          key={label}
+          size={size}
+          rounded={'none'}
+          bg={'black'}
+          color={'white'}
+          _hover={{ bg: 'white', color: 'black' }}
+          fontFamily={"Verdana Light"}
+          px={12}
+          py={8}
+          {...props}>
+          <Link href={href} _hover={{textDecoration: 'none'}}>
+            <Text>{label}</Text>
+          </Link>
+      </BaseButton>
+  )
 }
