80d023c1f3aa5d7aaf04dc67e1a72a6e2967cd38
venusnmj <venusnmj@hotmail.com>
2022-11-08 22:19:07 +0800

    comment in map stories removed

--- packages/ui/components/map/Map.stories.tsx
+++ packages/ui/components/map/Map.stories.tsx
@@ -6,7 +6,6 @@ import { Map } from './Map';
 export default {
     title: 'Components/Map',
     component: Map,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
     argTypes: {
 
     },
