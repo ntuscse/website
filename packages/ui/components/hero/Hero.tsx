f0c73f915a9ef3a4541866a32ffbb95dd041abb6
Jing Qiang <47933193+xJQx@users.noreply.github.com>
2022-11-10 13:07:09 +0800

    [SCSE-60] Add Hero component (#15)
    * add 2 primary button styles and test in storybook
    
    * remove size from button props + code styling
    
    * hero component with story
    
    * hero component without content; smaller hero component height in smaller viewport
    
    * fix linting errors
    
    * add key to buttons.map() to resolve warning
    
    * removed unnecessary variables

---
+++ packages/ui/components/hero/Hero.tsx
@@ -0,0 +1,74 @@
+import { Stack, Flex, Text, VStack, FlexProps, TextProps } from '@chakra-ui/react';
+import { Button, ButtonProps } from "ui"
+
+export interface HeroProps extends FlexProps {
+    backgroundImage: string
+    buttons?: Array<ButtonProps>
+    text?: string
+    textProps?: TextProps
+}
+
+export const Hero = ({ backgroundImage, buttons, text, textProps, ...flexProps }: HeroProps) => {
+    return (
+        // Background Image
+        <Flex
+            w='full'
+            h={{ base: '400px', md: '500px' }}
+            backgroundImage={ backgroundImage }
+            backgroundSize='cover'
+            backgroundPosition='center center'
+            { ...flexProps }>
+            <VStack
+                w='full'
+                justify='center'
+                px={{ base: 4, md: 8 }}
+                bgGradient={ 'linear(to-r, blackAlpha.600, transparent)' }>
+
+                {/* Content */}
+                <Stack align='center'
+                       justify='center'
+                       textAlign='center'
+                       spacing={ 6 }
+                       width={{ base: '75%', lg: '50%' }}
+                       height={{ base: '75%', lg: '50%' }}
+                       // White translucent box
+                       _before={ text || buttons ? { content: '""',
+                           bgColor:'white',
+                           opacity:'0.4',
+                           width: { base: '75%', lg: '50%' },
+                           height: { base: '75%', lg: '50%' },
+                           position: 'absolute',
+                           top: { base: '12.5%', lg: '25%' },
+                           right: { base: '12.5%', lg: '25%' } } : { content: '""' }}>
+                    {/* Text */}
+                    <Text
+                        color='black'
+                        fontWeight={ 700 }
+                        fontFamily='Verdana'
+                        lineHeight={ 1.2 }
+                        fontSize={{ base: '32px', md: '48px' }}
+                        zIndex={ 1 }
+                        { ...textProps }>
+                        { text }
+                    </Text>
+                    {/* Buttons */}
+                    <Stack direction={{ base: 'column', md: 'row' }} width='100%' justify='center'>
+                        { buttons?.map(button => {
+                            const { label, href, buttonType, ...buttonProps } = button;
+                            return (
+                                <Button
+                                    key={ label }
+                                    label={ label }
+                                    href={ href }
+                                    buttonType={ buttonType }
+                                    size='lg'
+                                    width={{ base: '100%', md: 'auto' }}
+                                    { ...buttonProps } />
+                            )
+                        })}
+                    </Stack>
+                </Stack>
+            </VStack>
+        </Flex>
+    );
+}
