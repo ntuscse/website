239ce04f10a5de18d6d897e53452ffa71f72a20b
venusnmj <venusnmj@hotmail.com>
2022-11-13 00:45:06 +0800

    edited map component for background and sizing

--- packages/ui/components/map/Map.tsx
+++ packages/ui/components/map/Map.tsx
@@ -11,7 +11,7 @@ export const Map = ({ title } : MapProps) => {
 const NtuMap = ({ title } : MapProps) => {
     return (
         <Flex
-            bgGradient='linear(to-t,#e6e3e3 48%,#121212 45%)'
+            bgGradient='linear(to-t,black 48%,transparent 45%)'
             filter='brightness( 100% ) contrast( 100% ) saturate( 0% ) blur( 0px ) hue-rotate( 0deg )'
             w='100%'
             m='0px'
@@ -20,8 +20,10 @@ const NtuMap = ({ title } : MapProps) => {
             justify="space-between"
             lineHeight='1'
             border='none'
+            alignItems='center'
+            justifyContent='center'
         >
-            <AspectRatio h='400px' w='100%'>
+            <AspectRatio h='400px' w='100%' maxW="1140px">
                 <iframe src="https://maps.google.com/maps?q=Nanyang%20Technological%20University&amp;t=m&amp;z=12&amp;output=embed&amp;iwloc=near" title={title} aria-label={title} />
             </AspectRatio>
         </Flex>
