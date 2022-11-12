7a9d8cbe1170009a4958c7e22b9964417f76d35f
venusnmj <venusnmj@hotmail.com>
2022-11-13 00:48:03 +0800

    added footer component, map component and other body and headings to contact page

--- apps/web/pages/contact.tsx
+++ apps/web/pages/contact.tsx
@@ -1,6 +1,53 @@
+import { Map, MapProps } from "ui";
+import { Heading, Box, Text, Link, Grid, GridItem } from '@chakra-ui/react';
+
 function Contact() {
+    const mapProps: MapProps = {
+        title: "Nanyang Technological University"
+    }
+
     return (
-        <div>Contact</div>
+        <div>
+            <Box px="20px" pb={ { base:'10px', md:'50px' } } pt={ { base:'50px' , md:'100px' } }textAlign="center">
+                <Heading
+                    as="h1"
+                    size='xl'
+                    textTransform="uppercase"
+                    fontWeight="900"
+                    fontFamily="Roboto, Sans-serif"
+                >
+                    Contact Us
+                </Heading>
+            </Box>
+            <Map title={mapProps.title}></Map>
+            <Box w="100%" px='20px' py={ { base:'40px', md:'80px' } } bg="black">
+                <Grid templateColumns={ { base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' } } maxW="1140px" mx='auto'>
+                    <GridItem p='20px' pb={ { base:'30px', md:'20px' } } textAlign={ { base:'center', md:'left' } }>
+                        <Heading as="h4" size='lg' color='white' mb={ { base:'5px', md:'20px' } }>Email</Heading>
+                        <Link color='white'>cse-it@e.ntu.edu.sg</Link>
+                    </GridItem>
+                    <GridItem p='20px' pb={ { base:'30px', md:'20px' } } textAlign={ { base:'center', md:'left' } }>
+                        <Heading as="h4" size='lg' color='white' mb={ { base:'5px', md:'20px' } }>Phone</Heading>
+                        <Text color='white'>+99 99999999</Text>
+                    </GridItem>
+                    <GridItem p='20px' pb={ { base:'30px', md:'20px' } } textAlign={ { base:'center', md:'left' } }>
+                        <Heading as="h4" size='lg' color='white' mb={ { base:'5px', md:'20px' } }>Address</Heading>
+                        <Text color='white'>NTU, Singapore</Text>
+                    </GridItem>
+                </Grid>
+            </Box>
+            <Box py={ { base:'40px', md:'80px' } } textAlign="center" px='20px'>
+                <Heading
+                    as="h2"
+                    size={ { base:'md', md:'lg' } }
+                    textTransform="uppercase"
+                    fontWeight="900"
+                    fontFamily="Roboto, Sans-serif"
+                >
+                    Frequently Asked Questions
+                </Heading>
+            </Box>
+        </div>
     )
 }
 
