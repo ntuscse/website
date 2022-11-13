e335e749c423bfada2766d124bfeb985709f3f94
Jing Qiang <tohjingqiang@gmail.com>
2022-11-13 16:36:01 +0800

    added all components to home page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,44 @@
+import { BlogCard, FooterContentButton, Hero, HeroProps } from "ui";
+import { Grid, GridItem, VStack } from "@chakra-ui/react";
+import { blogsData } from "@/pages/api/blogs";
+
 const Home = () => {
-  return (
-    <></>
-  )
+    const heroProps: HeroProps = {
+        backgroundImage: '/banners/scse-club-banner.png',
+        backgroundGradient: 'linear(to-r, whiteAlpha.500, whiteAlpha.500)',
+        text: 'WELCOME TO SCSE CLUB',
+        buttons: [
+            {
+                label: 'LEARN MORE',
+                href: '/events',
+                buttonType: 'primary.blue'
+            },
+            {
+                label: 'CONTACT US',
+                href: '/contact',
+                buttonType: 'primary.black'
+            }
+        ]
+    }
+
+    return (
+        <>
+            <Hero { ...heroProps } />
+            <VStack mx={{ base: 5, lg: 10 }}>
+                <Grid
+                    templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)', xl: 'repeat(3, 1fr)' }}
+                    gap={ 12 }
+                    pt={ 12 }
+                    pb={ 32 }>
+                    { blogsData.map(blogCardProps => (
+                        <GridItem key={ blogCardProps.blogCardContentProps.title }>
+                            <BlogCard { ...blogCardProps } />
+                        </GridItem>))}
+                </Grid>
+            </VStack>
+            <FooterContentButton href="./contact" label="Contact Us" title="Let’s work together or Need Help"/>
+        </>
+    )
 }
 
 export default Home
