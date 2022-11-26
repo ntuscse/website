74264567542fb4658e8875dabf3b270e0377d543
Dyllon <dyllon.g@outlook.com>
2022-11-26 11:39:34 +0800

    add custom slider

--- apps/web/pages/sponsors.tsx
+++ apps/web/pages/sponsors.tsx
@@ -1,4 +1,4 @@
-import { CarouselSpace, FooterContentButton } from "ui"
+import { Carousel, FooterContentButton } from "ui"
 import { sponsorsImageData } from "@/pages/api/sponsors";
 import { Divider, Heading, VStack } from "@chakra-ui/react";
 
@@ -8,7 +8,7 @@ const Sponsors = () => {
             <VStack pt={{ base: 5, lg: 10 }} minH='60vh'>
                 <Heading>OUR SPONSORS</Heading>
                 <Divider width='50%' marginTop='15px' marginBottom='15px' borderTop='1px' borderBottom='0px' borderColor='black'></Divider>
-                <CarouselSpace carouselItems={ sponsorsImageData.carouselItems }/>
+                <Carousel items={sponsorsImageData}/>
             </VStack>
             <FooterContentButton href="./contact" label="Contact Us" title="Wanna Sponsor us or work on a event together!"/>
         </>
