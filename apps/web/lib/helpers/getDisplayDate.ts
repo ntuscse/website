7df7bc797cf73c09d3a3b5434a9b1d6d9b1d4a4e
Jing Qiang <tohjingqiang@gmail.com>
2022-12-04 22:58:13 +0800

    fix getDisplayDate incorrect range

--- apps/web/lib/helpers/getDisplayDate.ts
+++ apps/web/lib/helpers/getDisplayDate.ts
@@ -1,8 +1,12 @@
 // Format date for display - e.g.: March 22, 2022
 export const getDisplayDate = (date: Date) => {
+  try {
     const dateObj = new Date(date)
     let displayDate = new Intl.DateTimeFormat("en-US", { month: "long", day: "2-digit" }).format(dateObj)
     displayDate += `, ${dateObj.getFullYear()}`
 
     return displayDate;
+  } catch {
+    return ''
+  }
 }
