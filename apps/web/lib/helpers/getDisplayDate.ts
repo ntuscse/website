009620c4b3862e2ef7449f2861d54acc14a9eb54
Jing Qiang <tohjingqiang@gmail.com>
2022-12-04 21:54:49 +0800

    blog page change to grid

---
+++ apps/web/lib/helpers/getDisplayDate.ts
@@ -0,0 +1,8 @@
+// Format date for display - e.g.: March 22, 2022
+export const getDisplayDate = (date: Date) => {
+    const dateObj = new Date(date)
+    let displayDate = new Intl.DateTimeFormat("en-US", { month: "long", day: "2-digit" }).format(dateObj)
+    displayDate += `, ${dateObj.getFullYear()}`
+
+    return displayDate;
+}
