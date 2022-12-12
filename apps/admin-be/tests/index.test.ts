baeeca9df19045b59864b81a9b0f003a02e61a6e
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2022-12-13 01:35:13 +0800

    fix: updated jest config

--- apps/admin-be/tests/index.test.ts
+++ apps/admin-be/tests/index.test.ts
@@ -1,11 +1,11 @@
-import app from "../src/app";
+import app from "../src/server";
 import request from "supertest";
 
 // example tests
 describe("register", () => {
   it("return status code 201", async () => {
-    await request(app)
-      .post("/auth/singin")
+    await request(app.app)
+      .get("/auth/signin")
       .send({ firstName: "Ivan" })
       .expect(201);
   });
