2a1ac3fe775ed01c9f4cb1bcb99d243c5cee4f21
rawang45 <csbhlim@gmail.com>
2023-01-28 00:46:40 +0800

    add oauth plugin to cms and make changes to .env.example in cms

--- apps/cms/src/payload.config.ts
+++ apps/cms/src/payload.config.ts
@@ -1,4 +1,6 @@
 import { buildConfig } from 'payload/config';
+import { oAuthPlugin } from 'payload-plugin-oauth';
+import axios from 'axios';
 import path from 'path';
 
 import Categories from './collections/Categories';
@@ -52,4 +54,29 @@ export default buildConfig({
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
+  plugins: [
+    oAuthPlugin({
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      authorizationURL: process.env.OAUTH_SERVER + '/oauth2/v2.0/authorize',
+      tokenURL: process.env.OAUTH_SERVER + '/oauth/token',
+      callbackURL: process.env.PAYLOAD_PUBLIC_SERVER_URL + '/oauth2/callback',
+      scope: 'basic',
+      async userinfo(accessToken) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const { data: user } = await axios.get(process.env.OAUTH_SERVER + '/oauth/me', {
+          params: { access_token: accessToken },
+        })
+        return {
+          sub: user.ID,
+
+          // Custom fields to fill in if user is created
+          name: user.display_name || user.user_nicename || 'Naamloos',
+          email: user.user_email,
+          role: user.capabilities?.administrator ? 'admin' : 'user',
+        }
+      },
+    }),
+  ],
+
 });
