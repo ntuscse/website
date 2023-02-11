000a9907aede75c66c80fc461bcf9c29874a9409
keykey <keyyew@gmail.com>
2023-02-11 22:30:11 +0800

    SCSE-149 added python script to import blogs onto payload CMS

---
+++ apps/cms/src/import/import_script.py
@@ -0,0 +1,55 @@
+import requests
+import json
+import json
+import xmltodict
+import re
+
+# reading the blog posts from blogs.json
+with open('blogs.json') as json_file:
+    blogs = json.load(json_file)
+
+
+# importing the blogs onto payload
+url = "http://localhost:3000/api/posts"
+
+for blog in blogs:
+    if blog == "12th-election-committee-election-nominees":
+        content = blogs[blog]["content"]
+        content = re.sub("<(“[^”]*”|'[^’]*’|[^'”>])*>", "", content)
+        content = content.replace("\n", " ")
+        payload = json.dumps({
+            "title": blogs[blog]["title"],
+            "author": "63e3b05e8ee6591800181b19",
+            "publishedDate": blogs[blog]["date"],
+            "tags": [],
+            "content": [{"children": [{
+                "text": content
+            }]},
+                {"children": [{
+                    "text": ""
+                }]},
+                {
+                "children": [{
+                    "type": "link",
+                    "linkType": "custom",
+                    "url": blogs[blog]["link"],
+                    "children": [
+                        {
+                            "text": blogs[blog]["link"]
+                        }
+                    ]
+                }]
+            }
+            ],
+            "status": "draft",
+            "createdAt": blogs[blog]["date"],
+            "updatedAt": blogs[blog]["date"]
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'Cookie': 'payload-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtleXlld0BnbWFpbC5jb20iLCJpZCI6IjYzZTNiMDVlOGVlNjU5MTgwMDE4MWIxOSIsImNvbGxlY3Rpb24iOiJ1c2VycyIsImlhdCI6MTY3NjEyMzk4MSwiZXhwIjoxNjc2MTMxMTgxfQ.qjERT1CsUSYINjs20O-XGMHHEVDHTVjn4NZFP68a6to'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
