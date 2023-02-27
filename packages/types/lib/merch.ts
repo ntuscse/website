d053b4cada3e138429fdd317aadc38f8861d3250
Dyllon <dyllon.g@outlook.com>
2023-02-27 14:57:00 +0800

    update merch types

--- packages/types/lib/merch.ts
+++ packages/types/lib/merch.ts
@@ -7,3 +7,20 @@ export interface Product {
 export interface Order {
   // todo
 }
+
+export interface Promotion {
+  promoCode: string;
+  discounts: Array<{
+    promoType: PromoType;
+    promoValue: number; // percent off or fixed value off based on promoType property
+    appliesTo: Array<string>; // array of product ids
+    minimumQty: number; // minimum quantity of items in the order to apply the discount
+    maxRedemptions: number;
+    redemptionsRemaining: number;
+  }>;
+}
+
+enum PromoType {
+  PERCENTAGE = "PERCENTAGE",
+  FIXED_VALUE = "FIXED_VALUE",
+}
