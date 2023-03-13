4005a5bf4d411a3b23b19bea4f26a7522ac57b46
Chan Wen Xu <lutherchanpublic@gmail.com>
2023-03-13 23:06:08 +0800

    feat: Implement orders and products endpoint
    [SCSE-236]
    [SCSE-237]

--- apps/merch/src/routes/products.ts
+++ apps/merch/src/routes/products.ts
@@ -1,17 +1,29 @@
 import { Router } from "express";
+import { getProduct, getProducts } from "../db";
 import { Product } from "types";
 
 const router = Router();
 
-const products: Product[] = [
-  {
-    id: "1",
-    name: "Sweater",
-  },
-];
+router.get("/", (req, res) => {
+  getProducts()
+    .then((products: Product[]) => {
+      res.json({ products });
+    })
+    .catch((e) => {
+      console.warn(e);
+      res.status(500).json({ error: "INTERNAL_SERVER_ERROR" });
+    });
+});
 
-router.get("/products", (req, res) => {
-  res.json({ products });
+router.get("/:id", (req, res) => {
+  getProduct(req.params.id)
+    .then((product: Product) => {
+      res.json(product);
+    })
+    .catch((e) => {
+      console.warn(e);
+      res.status(500).json({ error: "INTERNAL_SERVER_ERROR" });
+    });
 });
 
 export default router;
