c2a3c16157150a235754d9491fb3fb17c8b827a0
YoNG-Zaii <yongzaii811@outlook.com>
2023-03-21 01:18:43 +0800

    task: Port home page (product listing)

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,5 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "ui/theme";
 import "@fontsource/roboto/400.css";
@@ -8,13 +10,18 @@ import "@fontsource/roboto-slab/400.css";
 import "@fontsource/poppins/400.css";
 import { WebLayout } from "@/features/layout";
 
+const queryClient = new QueryClient({ defaultOptions: { queries: { refetchOnWindowFocus: false } } });
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={theme}>
-      <WebLayout>
-        <Component {...pageProps} />
-      </WebLayout>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <WebLayout>
+          <Component {...pageProps} />
+        </WebLayout>
+      </ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
---
+++ packages/ui/components/merch/Page.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+import { Flex, FlexProps, Box } from "@chakra-ui/react";
+import CartHeader from "./CartHeader";
+
+type PageProps = FlexProps & {
+  children: ReactNode;
+  hideHeader?: boolean;
+  contentWidth?: string;
+  contentPadding?: number[];
+};
+
+const Page = ({
+  children,
+  hideHeader = false,
+  contentWidth = "1400px",
+  contentPadding = [4, 6, 8],
+  ...props
+}: PageProps) => {
+  return (
+    <Flex flexDirection="column" {...props}>
+      {!hideHeader && <CartHeader />}
+      <Box
+        w="100%"
+        flexDir="column"
+        mt={[6, 10]}
+        mx="auto"
+        minHeight="75vh"
+        maxWidth={contentWidth}
+        px={contentPadding}
+      >
+        {children}
+      </Box>
+    </Flex>
+  );
+};
+
+export default Page;
