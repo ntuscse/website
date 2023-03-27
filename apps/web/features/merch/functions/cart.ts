23b3420f9a71ff1f3d72e43aca80deae50cec246
nicolelst <nicolelim2608@gmail.com>
2023-03-27 23:37:49 +0800

    feat(cart): add/uncomment utils/types/context for cart needed for MerchDetail page

---
+++ apps/web/features/merch/functions/cart.ts
@@ -0,0 +1,20 @@
+import { CartItem } from "types/lib/merch";
+
+export const getQtyInCart = (cartItems: CartItem[], productId: string, colorway: string, size: string) : number => {
+    const cartItem = cartItems.find((item) => {
+        return (item.productId === productId) && (item.size === size) && (item.colorway === colorway);
+    })
+
+    if (cartItem) {
+        return cartItem.quantity;
+    } 
+    return 0;
+}
+
+export const displayQtyInCart = (cartItems: CartItem[], productId: string, colorway: string, size: string) : string => {
+    const qty = getQtyInCart(cartItems, productId, colorway, size);
+    if (qty > 0) {
+        return `You have already added ${qty} to your cart.`;
+    }
+    return "";
+}
