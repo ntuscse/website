294a1092326586c479e3ebd12c4eebf075e40b09
Jing Qiang <tohjingqiang@gmail.com>
2023-05-26 14:42:14 +0800

    add empty join page

---
+++ apps/web/pages/join.tsx
@@ -0,0 +1,17 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+const Join = () => {
+  return (
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      minH="91vh"
+    >
+      <Text fontSize="3xl">
+        Coming Soon...
+      </Text>
+    </Flex>
+  );
+};
+
+export default Join;
