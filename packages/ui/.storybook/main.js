d1de9f2454cd003619b1b6578b407ca7cb6c9455
Dyllon <dyllon.g@outlook.com>
2023-05-31 22:42:39 +0800

    upgrade storybook to v7

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,27 +1,18 @@
 module.exports = {
-  stories: [
-    "../stories/introduction.stories.mdx",
-    "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)",
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: [
-    "@chakra-ui/storybook-addon",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "storybook-addon-next",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
+  stories: ["../stories/introduction.stories.mdx", "../components/**/*.stories.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)", "../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@chakra-ui/storybook-addon", "@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "storybook-addon-next", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
   features: {
-    emotionAlias: false,
+    emotionAlias: false
   },
   typescript: {
-    reactDocgen: "react-docgen",
+    reactDocgen: "react-docgen"
   },
   staticDirs: ["../assets"],
+  docs: {
+    autodocs: true
+  }
 };
