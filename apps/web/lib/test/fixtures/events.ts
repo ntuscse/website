29ab80c2631ef051ea551b53bb2a9786614df695
csbhlim222 <csbhlim@gmail.com>
2023-06-12 21:30:46 +0800

    test: add blog and footer test

---
+++ apps/web/lib/test/fixtures/events.ts
@@ -0,0 +1,70 @@
+import { BlogCardsDisplayProps } from "@/features/blogs";
+
+export const mockEvents: BlogCardsDisplayProps["posts"] = [
+  {
+    node: {
+      id: "cG9zdDoxNjk1",
+      title: "Techterview 101: Navigating the Internship World",
+      date: "2022-02-22T13:58:38",
+      uri: "/techterview-101-navigating-the-internship-world/",
+      slug: "techterview-101-navigating-the-internship-world",
+      excerpt:
+        "Techterview 101: Navigating the Internship World, a senior sharing event for the SCSE students took place on the 24th of",
+      author: {
+        node: {
+          id: "dXNlcjoz",
+          name: "acad",
+        },
+      },
+      featuredImage: {
+        node: {
+          link: "https://clubs.ntu.edu.sg/csec/techterview-101-navigating-the-internship-world/cover-photo/",
+        },
+      },
+    },
+  },
+  {
+    node: {
+      id: "cG9zdDoxNjk1",
+      title: "Techterview 101: Navigating the Internship World",
+      date: "2022-02-22T13:58:38",
+      uri: "/sce-members-day/",
+      slug: "sce-members-day",
+      excerpt:
+        "Dear SCE-ians, Interested to know more about Computer Engineering Club and win vouchers? Here you go! CEC will be bringi",
+      author: {
+        node: {
+          id: "dXNlcjozx",
+          name: "admin",
+        },
+      },
+      featuredImage: {
+        node: {
+          link: "https://clubs.ntu.edu.sg/csec/untitled/",
+        },
+      },
+    },
+  },
+  {
+    node: {
+      id: "cG9zdDo3NTA=",
+      title: "SCSE Dinner & Dance 2019",
+      date: "2020-01-15T20:25:28",
+      uri: "/scse-dinner-dance-2019/",
+      slug: "scse-dinner-dance-2019",
+      excerpt:
+        "SCSE Dinner &amp; Dance 2019 &nbsp; &nbsp; The School of Computer Science and Engineering (SCSE) hosted its annual Dinne",
+      author: {
+        node: {
+          id: "dXNlcjozx",
+          name: "admin",
+        },
+      },
+      featuredImage: {
+        node: {
+          link: "https://clubs.ntu.edu.sg/csec/scse-dinner-dance-2019/img_0971/",
+        },
+      },
+    },
+  },
+];
