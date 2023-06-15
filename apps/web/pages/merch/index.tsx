f5ee6ce78b256ebe64bc78c0e428854b106a060b
YoNG-Zaii <yongzaii811@outlook.com>
2023-06-15 21:17:15 +0800

    chore: Remove dummy data

--- apps/web/pages/merch/index.tsx
+++ apps/web/pages/merch/index.tsx
@@ -11,14 +11,11 @@ import { isOutOfStock } from "features/merch/functions/stock";
 const MerchandiseList = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>("");
   
-  const { data: products, isLoading } = { data: null, isLoading: false }
-  /*
   const { data: products, isLoading } = useQuery(
     [QueryKeys.PRODUCTS],
     () => api.getProducts(),
     {}
   );
-  */
 
   const categories = products?.map((product: Product) => product?.category);
   const uniqueCategories = categories
