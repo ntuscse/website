a5acf7cdd8d36638d79ecb555db79804348935fc
Joey Lim <jyorin0206@gmail.com>
2024-01-02 16:05:11 +0800

    added chakra ui to the project

--- apps/website/README.md
+++ apps/website/README.md
@@ -1,3 +1,7 @@
 ## set up instructions
 - run `yarn` to install dependencies
 - run `yarn dev` to start node server
+
+## Chakra UI
+- this app uses the Chakra UI component library
+- for more info, refer to https://chakra-ui.com/
--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -4,19 +4,23 @@ import Layout from './pages/Layout'
 import Home from './pages/Home'
 import Problems from './pages/Problems'
 import Profile from './pages/Profile'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="problems" element={<Problems />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="problems" element={<Problems />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+
   )
 }
 
