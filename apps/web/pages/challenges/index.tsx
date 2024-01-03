58990e89ed2f2c6c318a2c01d6a2a00d3854064b
Joey Lim <jyorin0206@gmail.com>
2024-01-03 14:57:48 +0800

    added dummy pages

---
+++ apps/web/pages/challenges/index.tsx
@@ -0,0 +1,13 @@
+import { Flex, Text } from "@chakra-ui/react"
+
+const Challenges = () => {
+    return (<Flex
+        minH="100vh"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center">
+        <Text>Challenges Home</Text>
+    </Flex>)
+}
+
+export default Challenges
