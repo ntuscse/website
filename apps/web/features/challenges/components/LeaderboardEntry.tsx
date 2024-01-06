8d8ccebe8a39a2302bc3b2d55cfbba11fff4c4e7
Joey Lim <jyorin0206@gmail.com>
2024-01-06 15:10:13 +0800

    added LeaderboardEntry component

---
+++ apps/web/features/challenges/components/LeaderboardEntry.tsx
@@ -0,0 +1,23 @@
+import { Flex, Text } from "@chakra-ui/react"
+
+interface LeaderboardEntryProps {
+    index: number,
+    name: string,
+    points: number
+}
+
+const LeaderboardEntry = ({ index, name, points }: LeaderboardEntryProps) => {
+    return (<Flex justify="space-evenly" py={1} >
+        <Flex justify="start" align="center" alignItems="center" w="80%">
+            <Text w="10%" textAlign="center">{index}</Text>
+            <Text w="90%" textAlign="start">{name}</Text>
+        </Flex>
+        <Flex w="20%" justify="end">
+            <Text>{points}</Text>
+        </Flex>
+    </Flex>
+    )
+
+}
+
+export default LeaderboardEntry
