328ef5fc7fc97b99480b4054bc456429770504d6
Joey Lim <jyorin0206@gmail.com>
2024-01-06 15:42:55 +0800

    added dropdown to pick season

--- apps/web/pages/challenges/leaderboard/index.tsx
+++ apps/web/pages/challenges/leaderboard/index.tsx
@@ -1,5 +1,19 @@
 import LeaderboardEntry from "@/features/challenges/components/LeaderboardEntry"
-import { Box, Flex, Text } from "@chakra-ui/react"
+import { Box, Flex, Select, Spacer, Text } from "@chakra-ui/react"
+import { useState } from "react"
+
+// placeholder for API Response
+let currentSeasonData: LeaderboardData[] = [
+    { uuid: 1000, userId: "alex200", points: 100 },
+    { uuid: 1001, userId: "johndoe010", points: 200 },
+    { uuid: 1002, userId: "janedoe345", points: 350 },
+]
+
+const seasonTwoData: LeaderboardData[] = [
+    { uuid: 1000, userId: "harvey562", points: 209 },
+    { uuid: 1001, userId: "pam391", points: 540 },
+    { uuid: 1002, userId: "linus903", points: 250 },
+]
 
 interface LeaderboardData {
     uuid: number,
@@ -7,24 +21,33 @@ interface LeaderboardData {
     points: number
 }
 const Leaderboard = () => {
-    // placeholder for API Response
-    const data: LeaderboardData[] = [
-        { uuid: 1000, userId: "alex200", points: 100 },
-        { uuid: 1001, userId: "johndoe010", points: 200 },
-        { uuid: 1002, userId: "janedoe345", points: 350 },
-    ]
+    const [currentDisplayedData, setCurrentDisplayedData] = useState(currentSeasonData)
+
+    function handleDataChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        // fetch and update leaderboard
+        switch (event.target.value) {
+            case 'season-id-1':
+                setCurrentDisplayedData(currentSeasonData)
+                break
+            case 'season-id-2': 
+                setCurrentDisplayedData(seasonTwoData)
+                break
+        }
+    
+    }
     return (<Flex
         minH="100vh"
         flexDirection="column"
         justifyContent="center"
         alignItems="center">
-        <Text textAlign="start" w="80vw">Leaderboard</Text>
+            <Select bg='#CBD5E0' borderColor='#CBD5E0' w="80vw" my={4} onChange={handleDataChange}>
+                <option value='season-id-1'>Current Season</option>
+                <option value='season-id-2'>Season 2</option>
+            </Select>
         <Box bg="#CBD5E0" w="80vw" px={8} py={4} minHeight="70vh" borderRadius="8">
-            {data.sort().reverse().map((item, index) => {
+            {currentDisplayedData.sort().reverse().map((item, index) => {
                 return <LeaderboardEntry key={item.uuid} index={index+1} name={item.userId} points={item.points}/>
             })}
-            
-      
         </Box>
     </Flex>)
 }
