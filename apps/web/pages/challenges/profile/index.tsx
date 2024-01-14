bf44e0897fb57690457710b6c7d4fa2eb65a12f5
Joey Lim <jyorin0206@gmail.com>
2024-01-14 16:37:40 +0800

    added profile interface and displayed user prof pic, name, email

--- apps/web/pages/challenges/profile/index.tsx
+++ apps/web/pages/challenges/profile/index.tsx
@@ -1,12 +1,25 @@
-import { Flex, Text } from "@chakra-ui/react"
+import { Avatar, Flex, Text } from "@chakra-ui/react"
+
+interface Profile {
+    username: string,
+    email: string,
+    profilePicUrl: string
+}
 
 const Profile = () => {
+    const user : Profile = {username: 'Eren Yeager', email: 'eren@aot.com', profilePicUrl: 'https://i.pinimg.com/474x/f8/6f/c4/f86fc4f39be083b5705a40de4c998b47.jpg'}
     return (<Flex
         minH="100vh"
-        flexDirection="column"
+        flexDir={['column', 'row']}
         justifyContent="center"
         alignItems="center">
-        <Text>Profile</Text>
+        <Flex px={4} flexDirection="column" justifyContent="center" alignItems="center" flex={1} minH="100vh">
+            <Avatar src={user.profilePicUrl} size={['xl', '2xl']}/>
+            <Text w={["100%", "50%"]} my={4}>Username: {user.username}</Text>
+            <Text w={["100%", "50%"]}>NTU email: {user.email}</Text>
+
+        </Flex>
+        <Flex flex={2} minH="100vh"></Flex>
     </Flex>)
 }
 
