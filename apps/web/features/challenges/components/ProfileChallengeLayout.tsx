7d49882dc2e1fced0602e1751450eabd23703ac9
Joey Lim <jyorin0206@gmail.com>
2024-01-14 22:01:27 +0800

    Create ProfileChallengeLayout.tsx

---
+++ apps/web/features/challenges/components/ProfileChallengeLayout.tsx
@@ -0,0 +1,17 @@
+import { Flex, Text, Button } from "@chakra-ui/react"
+
+interface ProfileChallengeLayoutProps {
+    challengeId: number,
+    challengeName: string,
+    onClick: () => void,
+    buttonText: string
+}
+const ProfileChallengeLayout = ({challengeId, challengeName, onClick, buttonText} : ProfileChallengeLayoutProps) => {
+    return <Flex mb={2} justifyContent="space-between" w={["85vw", "50vw"]} background="gray.200" p={4} borderRadius={8}>
+        <Text>{challengeId}. {challengeName}</Text>
+        <Button size="sm" onClick={() => {onClick()}}>{buttonText}</Button>
+    </Flex>
+
+}
+
+export default ProfileChallengeLayout
