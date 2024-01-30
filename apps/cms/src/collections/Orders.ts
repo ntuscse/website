565718f2bb5343bce312ee6399226eca29299315
Zhu Zhanyan <program.nom@gmail.com>
2024-01-30 20:43:03 +0800

    refactor(cms)!: generate Order collection fields using toPayloadZod()

--- apps/cms/src/collections/Orders.ts
+++ apps/cms/src/collections/Orders.ts
@@ -1,5 +1,7 @@
 import { CollectionConfig } from "payload/types";
-import Media from "./Media";
+import { OrderSchema } from "types";
+import { toPayloadZod } from "../utilities/zodInteropt";
+
 
 /** Orders collection stores merch orders from users. */
 const Orders: CollectionConfig = {
@@ -14,103 +16,6 @@ const Orders: CollectionConfig = {
     ],
     description: "Merchandise orders from users.",
   },
-  fields: [
-    // by default, payload generates an 'id' field each order automatically
-    {
-      name: "paymentGateway",
-      type: "text",
-      required: true,
-    },
-    {
-      name: "status",
-      label: "Order Status",
-      type: "select",
-      options: [
-        {
-          value: "pending",
-          label: "Pending Payment",
-        },
-        {
-          value: "paid",
-          label: "Payment Completed",
-        },
-        {
-          value: "delivered",
-          label: "Order Completed",
-        },
-      ],
-      required: true,
-    },
-    {
-      name: "customerEmail",
-      type: "email",
-      required: true,
-    },
-    {
-      name: "transactionID",
-      label: "Transaction ID",
-      admin: {
-        description: "Transaction ID provided by Payment Gateway",
-      },
-      type: "text",
-      required: true,
-    },
-    {
-      name: "orderDateTime",
-      label: "Ordered On",
-      type: "date",
-      admin: {
-        date: {
-          pickerAppearance: "dayAndTime",
-        },
-      },
-      required: true,
-    },
-    // ordered items for this order
-    {
-      name: "orderItems",
-      type: "array",
-      fields: [
-        {
-          name: "image",
-          type: "upload",
-          relationTo: Media.slug,
-          // validation: only allow image filetypes
-          filterOptions: {
-            mimeType: { contains: "image" },
-          },
-        },
-        {
-          name: "quantity",
-          type: "number",
-          required: true,
-        },
-        {
-          name: "size",
-          type: "text",
-          required: true,
-        },
-        {
-          name: "price",
-          type: "number",
-          required: true,
-        },
-        {
-          name: "name",
-          type: "text",
-          required: true,
-        },
-        {
-          name: "colorway",
-          type: "text",
-          required: true,
-        },
-      ],
-      // direct paylaod to generate a OrderItem type
-      interfaceName: "OrderItem",
-      // validate: orders should not be empty
-      minRows: 1,
-    },
-  ],
+  fields: toPayloadZod(OrderSchema),
 };
 export default Orders;
