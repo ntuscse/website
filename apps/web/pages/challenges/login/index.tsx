16421e7a3267380e4a13e933c21216f10416a9f1
Joey Lim <jyorin0206@gmail.com>
2024-02-06 18:38:11 +0800

    added input fields for login page

---
+++ apps/web/pages/challenges/login/index.tsx
@@ -0,0 +1,26 @@
+import { Button, Flex, FormControl, FormLabel, Heading, Input, Spacer, Text } from "@chakra-ui/react"
+
+const Login = () => {
+    return (<Flex minH="100vh" pt={24} flexDirection="column" alignItems="center" justifyContent="center">
+        <Flex w="50vw" h="50vh" p={8} flexDirection="column" alignItems="center" borderRadius={8} justifyContent="center"
+        >
+        <Heading>LOGIN</Heading>
+        <Text>Don't have an account? Sign up</Text>
+        <FormControl>
+            <FormLabel>Email</FormLabel>
+            <Input borderColor="black" type="email"/>
+        </FormControl>
+
+        <FormControl mt={2}>
+            <FormLabel>Password</FormLabel>
+            <Input borderColor="black" type="password"/>
+        </FormControl>
+        <Button my={10} w={"100%"} >Login</Button>
+        </Flex>
+        
+
+
+    </Flex>)
+}
+
+export default Login
