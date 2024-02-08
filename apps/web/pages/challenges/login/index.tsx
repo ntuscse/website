06aebf3072f29c5ddf2b6b9199967be0aff72ea8
Joey Lim <jyorin0206@gmail.com>
2024-02-08 21:11:42 +0800

    added login and password state and login btn fn

--- apps/web/pages/challenges/login/index.tsx
+++ apps/web/pages/challenges/login/index.tsx
@@ -1,22 +1,37 @@
 import { Button, Flex, FormControl, FormLabel, Heading, Input, Spacer, Text } from "@chakra-ui/react"
+import { useState } from "react"
+
+
 
 const Login = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    function handleLogin(username: string, password: string) {
+        // TODO: submit login details
+    }
     return (<Flex minH="100vh" pt={24} flexDirection="column" alignItems="center" justifyContent="center">
-        <Flex w={["100vw", "30vw"]} h="50vh" p={8} flexDirection="column" alignItems="center" borderRadius={8} justifyContent="center" boxShadow={["", "lg"]}>
-        <Heading>LOGIN</Heading>
-        <Text>Don't have an account? Sign up</Text>
-        <FormControl>
-            <FormLabel>Email</FormLabel>
-            <Input borderColor="black" type="email"/>
-        </FormControl>
+    
+            <Flex w={["100vw", "30vw"]} h="50vh" p={8} flexDirection="column" alignItems="center" borderRadius={8} justifyContent="center" boxShadow={["", "lg"]}>
 
-        <FormControl mt={2}>
-            <FormLabel>Password</FormLabel>
-            <Input borderColor="black" type="password"/>
-        </FormControl>
-        <Button my={10} w={"100%"} >Login</Button>
-        </Flex>
-        
+                <Heading>LOGIN</Heading>
+                <Text>Don't have an account? Sign up</Text>
+
+                <FormControl>
+                    <FormLabel>Email</FormLabel>
+                    <Input borderColor="black" type="email" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </FormControl>
+
+                <FormControl mt={2}>
+                    <FormLabel>Password</FormLabel>
+                    <Input borderColor="black" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </FormControl>
+                <Button my={10} w={"100%"} onClick={() => {handleLogin(username,password)}}>Login</Button>
+
+
+
+            </Flex>
+  
+
 
 
     </Flex>)
