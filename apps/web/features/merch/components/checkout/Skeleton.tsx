f1c41595e8bae92fd00b9ceb9de28a59f212264c
Chung Zhi Xuan <zhixuanchung03@gmail.com>
2024-03-18 21:51:49 +0800

    Bug fixes

--- apps/web/features/merch/components/checkout/Skeleton.tsx
+++ apps/web/features/merch/components/checkout/Skeleton.tsx
@@ -27,7 +27,7 @@ const CheckoutSkeleton: React.FC = () => {
         <Box borderWidth={1} borderRadius="lg" p={[4, 6]} boxShadow="md">
           <Skeleton width="50%" height={6} />
           <Skeleton width="65%" height={8} mt={2} mb={4} />
-          <ItemSkeleton />
+          <ItemSkeleton /> as JSX.Element
           <ItemSkeleton /> as JSX.Element
           <ItemSkeleton />
         </Box>
