09e05e35bd5fffa7cb083ce349f5a7725edc7ce3
jack-thant <thanthtoo5728@gmail.com>
2024-07-06 21:03:20 +0800

    Add infinite horizontal scroll to the gallery-grid section

--- packages/ui/components/gallery-grid/GalleryGrid.tsx
+++ packages/ui/components/gallery-grid/GalleryGrid.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex, Image, Text, Grid, GridItem } from '@chakra-ui/react';
+import { Flex, Box, Image, Text, Grid, GridItem, keyframes } from '@chakra-ui/react';
 
 export interface GalleryGridProps {
     title: string,
@@ -13,19 +13,38 @@ export type Image = {
     colSpan?: number,
 }
 
+const scroll = keyframes`
+  0% { transform: translate(0); }
+  100% { transform: translate(-100%); }
+`;
+
 export const GalleryGrid = ({ title, images }: GalleryGridProps) => {
     return (
         <Flex align='center' justify='center' flexDir='column' gap={8} py={20}>
             <Text fontSize='5xl' textAlign={'center'}>{title}</Text>
-            <Grid h={768} templateRows='repeat(2,1fr)' templateColumns='repeat(5, 1fr)' gap={3}>
-                {
-                    images.map(({ src, alt, colSpan, rowSpan }) => (
-                        <GridItem key={src} colSpan={colSpan} rowSpan={rowSpan}>
-                            <Image src={ src } alt={ alt } objectFit="cover" width="100%" height="100%" borderRadius={10}/>
-                        </GridItem>
-                    ))
-                }
-            </Grid>
+            <Box overflow='hidden' width='100vw' position='relative' height='768px'>
+                <Flex position='absolute' gap={3}>
+                    <Grid h={768} width='100vw' templateRows='repeat(2,1fr)' templateColumns='repeat(5, 1fr)' gap={3} animation={`${scroll} 80s linear infinite backwards`}>
+                        {
+                            images.map(({ src, alt, colSpan, rowSpan }) => (
+                                <GridItem key={src} colSpan={colSpan} rowSpan={rowSpan}>
+                                    <Image src={src} alt={alt} objectFit="cover" width="100%" height="100%" borderRadius={10} />
+                                </GridItem>
+                            ))
+                        }
+                    </Grid>
+                    <Grid width='100vw' h={768} templateRows='repeat(2,1fr)' templateColumns='repeat(5, 1fr)' gap={3} animation={`${scroll} 80s linear infinite backwards`}>
+                        {
+                            images.map(({ src, alt, colSpan, rowSpan }) => (
+                                <GridItem key={src} colSpan={colSpan} rowSpan={rowSpan}>
+                                    <Image src={src} alt={alt} objectFit="cover" width="100%" height="100%" borderRadius={10} />
+                                </GridItem>
+                            ))
+                        }
+                    </Grid>
+                </Flex>
+            </Box>
+
         </Flex>
     )
 }
