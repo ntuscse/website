9f8531752b09a1e2c7b9686f667cf46ad649198d
limivann <71662324+limivann@users.noreply.github.com>
2024-08-27 21:56:47 +0800

    Fix lint

--- apps/cms/src/apis/orders.api.ts
+++ apps/cms/src/apis/orders.api.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/require-await */
 import { Order } from "../@types/Order";
 
 class OrdersApi {
-  // eslint-disable-next-line @typescript-eslint/require-await
   async getOrders(): Promise<Order[]> {
     const req = await fetch(`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api/orders`);
     const orders = await req.json();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     return orders?.docs as Order[];
   }
 }
