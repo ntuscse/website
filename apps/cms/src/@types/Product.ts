d0c3ff4ad6ae21a4740d766d4f4f4aaab7a34f4e
ZhiXuan <zhixuanchung03@gmail.com>
2024-09-13 23:06:14 +0800

    Implemented edit and delete feature for products

---
+++ apps/cms/src/@types/Product.ts
@@ -0,0 +1,10 @@
+export class Product {
+    constructor(
+        public id = '',
+        public name = '',
+        public colors= '',
+        public sizes = '',
+        public price = '',
+        public category = ''
+    ) { }
+}
